6376c0fe138a81e56ccc2be1b6bb0612fac29d6b
AndrewMochalskyi <42970249+AndrewMochalskyi@users.noreply.github.com>
2020-02-10 00:19:07 +0200

    Rain

---
+++ Assets/Bandit/Scripts/PlayerCombat.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+    void Update()
+    {
+           if(Input.GetKeyDown(KeyCode.F))
+           {
+            Attack();
+           }
+    }
+
+
+    void Attack()
+    {
+        animator.SetTrigger("Attack");
+    }
+
+}
